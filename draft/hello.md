09ccef3f1186a919a8581e94a6070883b94a7541
rabix@rabix <rabix@rabix>
2022-06-24 03:20:07 +0800

    Hello world

---
+++ draft/hello.md
@@ -0,0 +1,24 @@
+# Hello world for Scala
+### Install
+https://www.scala-lang.org/download/
+
+### Test
+```scala
+/// <a.scala>
+object Hello {
+    def main(args: Array[String]) = {
+        println("Hello world")
+    }
+    
+}
+```
+
+```bash
+scala a.scala
+```
+
+### Play the book
+Using `runmd.sh hello.md`
+
+### Other resources
+https://docs.scala-lang.org/
---
+++ draft/runmd.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+ruby -e '
+lines = []
+fopen = 0
+File.read(ARGV[0]).split("\n").each{|line|
+    if line.strip == "```scala"
+       fopen = 1
+    elsif line.strip == "```bash"
+       fopen = 2
+    elsif line.strip == "```"
+       if fopen == 1
+          r = lines[0][/<(.+)>/, 1]
+          File.write r, lines[0..-1].join("\n") + "\n"
+       elsif fopen == 2
+          File.write "/tmp/runmd.sh", lines[0..-1].join("\n") + "\n"
+          system "chmod +x /tmp/runmd.sh"
+          system "/tmp/runmd.sh"
+       else
+          puts "Unknown"
+       end
+       lines = []
+       fopen = 0
+    else
+       if fopen == 1 || fopen == 2
+          lines << line
+       end
+    end
+}
+' $1
+
